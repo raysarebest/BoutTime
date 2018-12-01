dd56324be02d93f97041452e628439f41ddcd0d3
Michael Hulet <michael@hulet.tech>
2018-11-30 21:54:14 -0600

    Created initial project files

---
+++ 'Bout Time/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  'Bout Time
+//
+//  Created by Michael Hulet on 11/30/18.
+//  Copyright © 2018 Michael Hulet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
