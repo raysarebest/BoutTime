f8619bb7db6db1bf6893573ae808642760063f6e
Michael Hulet <michael@hulet.tech>
2018-12-02 00:54:01 -0600

    Added Event model

---
+++ 'Bout Time/Event.swift
@@ -0,0 +1,20 @@
+//
+//  Event.swift
+//  'Bout Time
+//
+//  Created by Michael Hulet on 12/2/18.
+//  Copyright © 2018 Michael Hulet. All rights reserved.
+//
+
+import Foundation
+struct Event: Decodable{
+    let title: String
+    let moment: Date
+    let infoLink: URL
+}
+
+extension Event: Comparable{
+    static func < (left: Event, right: Event) -> Bool{
+        return left.moment < right.moment
+    }
+}
